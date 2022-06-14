26f0eae52892a94a2ad4a9c480c6f27eb4107967
Shreya Agarwal <93423387+Shreyaaga@users.noreply.github.com>
2022-06-14 16:29:39 +0530

    BeautifulSoup

--- .ipynb_checkpoints/webscaping.py
+++ .ipynb_checkpoints/webscaping.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 url="https://www.codewithharry.com/"
-
 # get the html
 r=requests.get(url)   # brings plain content
 htmlcontent=r.content
@@ -15,5 +14,29 @@ soup=BeautifulSoup(htmlcontent, 'html.parser')
 
 
 # html tree traversal
+
+# get the title of the html page 
 title=soup.title
 print(title)
+# to print all paragraph tags
+paras=soup.find_all('p')
+print(paras)
+# to get classes of first paragraph
+para=soup.find('p')['class']
+print(para)
+# find all the elements with class lead
+print(soup.find_all("p",class_='text-sm'))
+# get the text from the tage
+print(soup.find('p').get_text())
+# get al anchor tags and links within it
+anchor=soup.find_all('a')
+for link in anchor:
+     print(link.get('href')) #.get is used to grab teh particular attribute
+#childer
+ul = soup.find("ul")
+for i in ul.children:
+	print(i)
+
+#previous child
+ul = soup.find(id="li")
+print(ul.next_sibling.next_sibling)
